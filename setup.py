a2de65bab8fb56a49e4202e473977070568e1674
Sebastian Junges <sebastian.junges@gmail.com>
2020-04-25 20:02:20 -0700

    some cleaning from the initial project template

--- setup.py
+++ setup.py
@@ -8,16 +8,15 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'))
 setup(
     name="storm-project-starter-python",
     version="0.1",
-    author="M. Volk",
-    author_email="matthias.volk@cs.rwth-aachen.de",
+    author="Sebastian Junges",
+    author_email="sebastian.junges@cs.rwth-aachen.de",
     maintainer="S. Junges",
     maintainer_email="sebastian.junges@cs.rwth-aachen.de",
-    url="https://github.com/moves-rwth/storm-project-starter-python",
-    description="Starter project for the Python API of Storm via Stormpy",
+    description="Shielding on POMDPs with Storm",
     long_description=long_description,
     long_description_content_type='text/markdown',
 
-    packages=["stormpy_starter"],
+    packages=["rlshield"],
     install_requires=[
         "stormpy>=1.3.0"
     ],
