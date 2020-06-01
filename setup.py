4993de98da9c86f449dddd4e47425843137cdd81
Sebastian Junges <sebastian.junges@gmail.com>
2020-06-01 11:11:26 -0700

    cleaning

--- setup.py
+++ setup.py
@@ -6,19 +6,15 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'))
     long_description = f.read()
 
 setup(
-    name="storm-project-starter-python",
+    name="rlshield",
     version="0.1",
-    author="Sebastian Junges",
-    author_email="sebastian.junges@cs.rwth-aachen.de",
-    maintainer="S. Junges",
-    maintainer_email="sebastian.junges@cs.rwth-aachen.de",
     description="Shielding on POMDPs with Storm",
     long_description=long_description,
     long_description_content_type='text/markdown',
 
     packages=["rlshield"],
     install_requires=[
-        "stormpy>=1.3.0"
+        "stormpy>=1.4.0"
     ],
     python_requires='>=3',
 )
