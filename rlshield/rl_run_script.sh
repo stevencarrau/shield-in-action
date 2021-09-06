9fd55a86057285dfd7fd67b10c50c34698991e15
Ubuntu <ubuntu@ip-172-31-40-3.us-east-2.compute.internal>
2021-09-06 00:29:42 +0000

    Variable length cleaning

--- rlshield/rl_run_script.sh
+++ rlshield/rl_run_script.sh
@@ -1,4 +1,4 @@
-source ~/storm_env/bin/activate
+source ~/stormenv/bin/activate
 
 declare -i MAX_STEPS=5000
 declare -i EVAL_EPISODES=10
@@ -6,7 +6,7 @@ declare -i EVAL_INTERVAL=100
 declare -a hold_ind=("0" "1")
 declare -a Environments=("refuel" "refuel")
 declare -a EpisodeLengths=(5 10 25 50 100 150 200 250)
-declare -a Constants=("N=6,ENERGY=7" "N=6,ENERGY=8" "N=6")
+declare -a Constants=("N=6,ENERGY=7" "N=6,ENERGY=8")
 declare -a LearningMethods=("REINFORCE")
 
 
@@ -15,7 +15,7 @@ echo ${Constants[$i]}
 echo ${Environments[$i]}
 for j in "${!EpisodeLengths[@]}"; do
 echo ${EpisodeLengths[$j]}
-	python shield.py --grid-model ${Environments[$i]} --constants ${Constants[$i]} --video-path newvideos/REINFORCE -NN $MAX_STEPS --obs_level "BELIEF_SUPPORT" --valuations "False" --learning_method ${LearningMethods[$j]} --eval-interval $EVAL_INTERVAL  --eval-episodes $EVAL_EPISODES -s ${EpisodeLengths[$j]} 
-	python shield.py --grid-model ${Environments[$i]} --constants ${Constants[$i]} --video-path newvideos/REINFORCE -NN $MAX_STEPS --obs_level "BELIEF_SUPPORT" --valuations "False" --learning_method "REINFORCE" --eval-interval $EVAL_INTERVAL --eval-episodes $EVAL_EPISODES --noshield -s ${EpisodeLengths[$j]}
+	python shield.py --grid-model ${Environments[$i]} --constants ${Constants[$i]} --video-path newvideos/REINFORCE -NN $MAX_STEPS --obs_level "BELIEF_SUPPORT" --learning_method "REINFORCE" --eval-interval $EVAL_INTERVAL  --eval-episodes $EVAL_EPISODES -s ${EpisodeLengths[$j]} 
+	python shield.py --grid-model ${Environments[$i]} --constants ${Constants[$i]} --video-path newvideos/REINFORCE -NN $MAX_STEPS --obs_level "BELIEF_SUPPORT" --learning_method "REINFORCE" --eval-interval $EVAL_INTERVAL --eval-episodes $EVAL_EPISODES --noshield -s ${EpisodeLengths[$j]}
 done
 done
--- rlshield/rl_simulator.py
+++ rlshield/rl_simulator.py
@@ -194,7 +194,6 @@ class TF_Environment(SimulationExecutor):
         return current_step
 
     def simulate_deep_RL(self, recorder, total_nr_runs=5,eval_interval=1000,eval_episodes=10, maxsteps=30,eval_env=None,agent_arg='DQN',log_name=None):
-        logfile = open(log_name,"w")
         self.maxsteps = maxsteps
         gamma = 1.0
         alpha = 3e-2
@@ -244,9 +243,8 @@ class TF_Environment(SimulationExecutor):
                 avg_return = compute_avg_return(eval_env, RL_agent.agent, RL_agent.agent.policy, num_eval_episodes,max_steps=maxsteps)
                 print('step = {0}: Average Return = {1}'.format(step, avg_return))
                 returns.append((step,avg_return))
-                logfile.write('{0} {1}\n'.format(step,avg_return))
+              
         record_track(recorder,eval_env,RL_agent.agent,RL_agent.agent.policy,maxsteps)
-        logfile.close()
         return returns
 
     def observe(self):
